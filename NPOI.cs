8922d6a63d2f29a9c5d40d18cc9730fb10a4c6e5
Zaib92 <80528971+Zaib92@users.noreply.github.com>
2021-04-09 13:28:55 +0100

    Added and styled merged cells

--- NPOI.cs
+++ NPOI.cs
@@ -6,6 +6,9 @@ using NPOI.XSSF.UserModel;
 using System.Data;
 using System.Linq;
 using System.Drawing;
+using NPOI.SS.Util;
+using System.Windows;
+using NPOI.HSSF.Util;
 
 namespace BenchmarkingExcelPackages
 {
@@ -46,7 +49,9 @@ namespace BenchmarkingExcelPackages
             return dataTable;
         }
 
-        // Write excel
+
+
+        //Write excel
         public void WriteData()
         {
             DataTable table = ImportData();
@@ -82,31 +87,39 @@ namespace BenchmarkingExcelPackages
             }
 
             // get a row in this case 1 ... Create a cell at column 6 (no data persists here currently hence create)
-            ICell cell = sheet.GetRow(1).CreateCell(5);
 
-            // styling
             ICellStyle style = workbook.CreateCellStyle();
             IFont font = workbook.CreateFont();
+            //Loops styling
+            for (var i = 1; i <= sheet.LastRowNum; i++)
+            {
+                var cell = sheet.GetRow(i).CreateCell(5);
+                if (cell == null)
+                {
+                    continue;
+                }
 
-            // set up a font...
-            font.IsBold = true;
-            font.FontName = "WingDings";
-            style.SetFont(font);
+                font.IsBold = true;
+                font.FontName = "WingDings";
+                style.SetFont(font);
 
-            //NOTE: this is a bit weird, but it works (setting FillForegroundColor + FillPattern sets up the background...)
-            style.FillForegroundColor = IndexedColors.Red.Index;
-            style.FillPattern = FillPattern.SolidForeground;
+                style.FillForegroundColor = IndexedColors.Red.Index;
+                style.FillPattern = FillPattern.SolidForeground;
 
-            //NOTE: seem to need to deal with all sides of a cell border
-            style.BorderBottom = BorderStyle.Double;
-            style.BorderLeft = BorderStyle.Double;
-            style.BorderTop = BorderStyle.Double;
-            style.BorderRight = BorderStyle.Double;
+                cell.SetCellValue("ü");
+                cell.CellStyle = style;
+            }
 
+            //Creates single border around merged cells.
+            sheet.GetRow(0);
+            CellRangeAddress region = new CellRangeAddress(0, 0, 6, 8);
+            sheet.AddMergedRegion(region);
 
-            // set the above styles and add the value that is a tick in wingdings to the selected cell...
-            cell.SetCellValue("ü");
-            cell.CellStyle = style;
+            //Note: The first parameter 1 indicates the thickness of the border  
+            RegionUtil.SetBorderBottom(1, region, sheet);//Bottom border  
+            RegionUtil.SetBorderLeft(1, region, sheet);//Left border  
+            RegionUtil.SetBorderRight(1, region, sheet);//Right border  
+            RegionUtil.SetBorderTop(1, region, sheet);//top border
 
 
             string path = "";
@@ -116,166 +129,7 @@ namespace BenchmarkingExcelPackages
                 workbook.Write(fileStream);
             }
         }
+
     }
+
 }
-
-
-//class XXX
-//{
-//    public int Cell { get; set; }
-//    public string Value { get; set; }
-//}
-
-//private static void ImportExcel()
-//{
-
-//    var newFile = "newbook2.core.xlsx";
-//    var celldata = new List<XXX>{
-//                new XXX{ Cell =0,Value="00000"},
-//                new XXX{ Cell = 1,Value = "1111111"  }
-//            };
-
-//    using (var fs = new FileStream(newFile, FileMode.Create, FileAccess.Write))
-//    {
-//        //excelPath
-//        IWorkbook wb = new XSSFWorkbook("newbook.core.xlsx");
-
-//        ISheet sheet1 = wb.GetSheetAt(0);
-
-//        //celldata        
-//        foreach (var x in celldata)
-//        {
-//            IRow row = sheet1.GetRow(x.Cell);
-//            row.GetCell(x.Cell).SetCellValue(x.Value);
-//        }
-//        wb.Write(fs);
-//    }
-//}
-
-//private static void ExportExcelHSSF()
-//{
-//    var newFile = @"newbook.core.xls";
-
-//    using (var fs = new FileStream(newFile, FileMode.Create, FileAccess.Write))
-//    {
-//        IWorkbook workbook = new HSSFWorkbook();
-//        ISheet sheet1 = workbook.CreateSheet("Sheet1");
-//        sheet1.AddMergedRegion(new CellRangeAddress(0, 0, 0, 10));
-//        //ICreationHelper cH = wb.GetCreationHelper();
-//        var rowIndex = 0;
-//        IRow row = sheet1.CreateRow(rowIndex);
-//        row.Height = 30 * 80;
-//        var cell = row.CreateCell(0);
-//        var font = workbook.CreateFont();
-//        font.IsBold = true;
-//        font.Color = HSSFColor.DarkBlue.Index2;
-//        cell.CellStyle.SetFont(font);
-
-//        cell.SetCellValue("A very long piece of text that I want to auto-fit innit, yeah. Although if it gets really, really long it'll probably start messing up more.");
-//        sheet1.AutoSizeColumn(0);
-//        rowIndex++;
-
-//        // create sheet
-//        var sheet2 = workbook.CreateSheet("My Sheet");
-//        // create cell styles?
-//        var style1 = workbook.CreateCellStyle();
-//        style1.FillForegroundColor = HSSFColor.Blue.Index2;
-//        style1.FillPattern = FillPattern.SolidForeground;
-
-//        var style2 = workbook.CreateCellStyle();
-//        style2.FillForegroundColor = HSSFColor.Yellow.Index2;
-//        style2.FillPattern = FillPattern.SolidForeground;
-
-//        // format cells?
-//        var cell2 = sheet2.CreateRow(0).CreateCell(0);
-//        cell2.CellStyle = style1;
-//        cell2.SetCellValue(0);
-
-//        cell2 = sheet2.CreateRow(1).CreateCell(0);
-//        cell2.CellStyle = style2;
-//        cell2.SetCellValue(1);
-
-//        cell2 = sheet2.CreateRow(2).CreateCell(0);
-//        cell2.CellStyle = style1;
-//        cell2.SetCellValue(2);
-
-//        cell2 = sheet2.CreateRow(3).CreateCell(0);
-//        cell2.CellStyle = style2;
-//        cell2.SetCellValue(3);
-
-//        cell2 = sheet2.CreateRow(4).CreateCell(0);
-//        cell2.CellStyle = style1;
-//        cell2.SetCellValue(4);
-
-//        workbook.Write(fs);
-//    }
-//    Console.WriteLine("Excel  Done");
-//}
-//private static void ExportExcel()
-//{
-//    var newFile = @"newbook.core.xlsx";
-
-//    using (var fs = new FileStream(newFile, FileMode.Create, FileAccess.Write))
-//    {
-//        IWorkbook workbook = new XSSFWorkbook();
-//        ISheet sheet1 = workbook.CreateSheet("Sheet1");
-//        sheet1.AddMergedRegion(new CellRangeAddress(0, 0, 0, 10));
-//        //ICreationHelper cH = wb.GetCreationHelper();
-//        var rowIndex = 0;
-//        IRow row = sheet1.CreateRow(rowIndex);
-//        row.Height = 30 * 80;
-//        var cell = row.CreateCell(0);
-//        var font = workbook.CreateFont();
-//        font.IsBold = true;
-//        font.Color = HSSFColor.DarkBlue.Index2;
-//        cell.CellStyle.SetFont(font);
-
-//        cell.SetCellValue("A very long piece of text that I want to auto-fit innit, yeah. Although if it gets really, really long it'll probably start messing up more.");
-//        sheet1.AutoSizeColumn(0);
-//        rowIndex++;
-
-//        // 新增試算表。
-//        var sheet2 = workbook.CreateSheet("My Sheet");
-//        // 建立儲存格樣式。
-//        var style1 = workbook.CreateCellStyle();
-//        style1.FillForegroundColor = HSSFColor.Blue.Index2;
-//        style1.FillPattern = FillPattern.SolidForeground;
-
-//        var style2 = workbook.CreateCellStyle();
-//        style2.FillForegroundColor = HSSFColor.Yellow.Index2;
-//        style2.FillPattern = FillPattern.SolidForeground;
-
-//        // 設定儲存格樣式與資料。
-//        var cell2 = sheet2.CreateRow(0).CreateCell(0);
-//        cell2.CellStyle = style1;
-//        cell2.SetCellValue(0);
-
-//        cell2 = sheet2.CreateRow(1).CreateCell(0);
-//        cell2.CellStyle = style2;
-//        cell2.SetCellValue(1);
-
-//        cell2 = sheet2.CreateRow(2).CreateCell(0);
-//        cell2.CellStyle = style1;
-//        cell2.SetCellValue(2);
-
-//        cell2 = sheet2.CreateRow(3).CreateCell(0);
-//        cell2.CellStyle = style2;
-//        cell2.SetCellValue(3);
-
-//        cell2 = sheet2.CreateRow(4).CreateCell(0);
-//        cell2.CellStyle = style1;
-//        cell2.SetCellValue(4);
-
-//        workbook.Write(fs);
-//    }
-//    Console.WriteLine("Excel  Done");
-
-//}
-//    }
-//}
-
-
-
-
-
-
